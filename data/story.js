d1deceab5e878865980221833b7fb7db9ecfdc23
Leandro Carlos Pereira <75700750+lcpereira@users.noreply.github.com>
2023-06-28 12:04:34 -0300

    Add files via upload

--- data/story.js
+++ data/story.js
@@ -60,12 +60,32 @@ var story = {
       },
       {
        "rect": {
+        "x": 30.22216796875,
+        "y": 302.22216796875,
+        "width": 163.5558319091797,
+        "height": 21.33333396911621
+       },
+       "index": 2,
+       "page": 7
+      },
+      {
+       "rect": {
+        "x": 30.22216796875,
+        "y": 194.666748046875,
+        "width": 181.33360290527344,
+        "height": 21.33333396911621
+       },
+       "index": 3,
+       "page": 6
+      },
+      {
+       "rect": {
         "x": 20.4443359375,
         "y": 127.111083984375,
         "width": 233.77777099609375,
         "height": 35.55555725097656
        },
-       "index": 2,
+       "index": 4,
        "page": 1
       },
       {
@@ -75,8 +95,8 @@ var story = {
         "width": 234,
         "height": 52
        },
-       "index": 3,
-       "url": "https://www.google.com.br/"
+       "index": 5,
+       "url": "https://www.plataforma.ze.express/app/sales"
       },
       {
        "rect": {
@@ -85,7 +105,7 @@ var story = {
         "width": 234,
         "height": 52
        },
-       "index": 4,
+       "index": 6,
        "url": "https://conteudo.ze.express/abf-c"
       }
      ],
@@ -153,7 +173,7 @@ var story = {
         "width": 146.6666717529297,
         "height": 22.22222137451172
        },
-       "index": 8,
+       "index": 10,
        "page": 8
       },
       {
@@ -163,7 +183,7 @@ var story = {
         "width": 163.5558319091797,
         "height": 21.33333396911621
        },
-       "index": 9,
+       "index": 11,
        "page": 7
       },
       {
@@ -173,7 +193,7 @@ var story = {
         "width": 181.33360290527344,
         "height": 21.33333396911621
        },
-       "index": 10,
+       "index": 12,
        "page": 6
       },
       {
@@ -183,7 +203,7 @@ var story = {
         "width": 233.77777099609375,
         "height": 35.55555725097656
        },
-       "index": 11,
+       "index": 13,
        "page": 1
       },
       {
@@ -193,8 +213,8 @@ var story = {
         "width": 234,
         "height": 52
        },
-       "index": 12,
-       "url": "https://www.google.com.br/"
+       "index": 14,
+       "url": "https://www.plataforma.ze.express/app/sales"
       },
       {
        "rect": {
@@ -203,7 +223,7 @@ var story = {
         "width": 234,
         "height": 52
        },
-       "index": 13,
+       "index": 15,
        "url": "https://conteudo.ze.express/abf-c"
       }
      ],
@@ -219,7 +239,7 @@ var story = {
       "width": 274.74822998046875,
       "height": 46.908233642578125
      },
-     "index": 5,
+     "index": 7,
      "page": 4,
      "pageType": "overlay",
      "pageOverlayPin": 0,
@@ -234,7 +254,7 @@ var story = {
       "width": 254.01416015625,
       "height": 48.583526611328125
      },
-     "index": 6,
+     "index": 8,
      "page": 2
     },
     {
@@ -244,7 +264,7 @@ var story = {
       "width": 292.60498046875,
       "height": 48.583526611328125
      },
-     "index": 7,
+     "index": 9,
      "page": 3
     }
    ],
@@ -291,7 +311,7 @@ var story = {
         "width": 146.6666717529297,
         "height": 22.22222137451172
        },
-       "index": 16,
+       "index": 18,
        "page": 8
       },
       {
@@ -301,7 +321,7 @@ var story = {
         "width": 163.5558319091797,
         "height": 21.33333396911621
        },
-       "index": 17,
+       "index": 19,
        "page": 7
       },
       {
@@ -311,7 +331,7 @@ var story = {
         "width": 181.33360290527344,
         "height": 21.33333396911621
        },
-       "index": 18,
+       "index": 20,
        "page": 6
       },
       {
@@ -321,7 +341,7 @@ var story = {
         "width": 233.77777099609375,
         "height": 35.55555725097656
        },
-       "index": 19,
+       "index": 21,
        "page": 1
       },
       {
@@ -331,8 +351,8 @@ var story = {
         "width": 234,
         "height": 52
        },
-       "index": 20,
-       "url": "https://www.google.com.br/"
+       "index": 22,
+       "url": "https://www.plataforma.ze.express/app/sales"
       },
       {
        "rect": {
@@ -341,7 +361,7 @@ var story = {
         "width": 234,
         "height": 52
        },
-       "index": 21,
+       "index": 23,
        "url": "https://conteudo.ze.express/abf-c"
       }
      ],
@@ -357,7 +377,7 @@ var story = {
       "width": 254.01416015625,
       "height": 48.583526611328125
      },
-     "index": 14,
+     "index": 16,
      "page": 1
     },
     {
@@ -367,7 +387,7 @@ var story = {
       "width": 292.60498046875,
       "height": 48.583526611328125
      },
-     "index": 15,
+     "index": 17,
      "page": 3
     }
    ],
@@ -414,7 +434,7 @@ var story = {
         "width": 146.6666717529297,
         "height": 22.22222137451172
        },
-       "index": 24,
+       "index": 26,
        "page": 8
       },
       {
@@ -424,7 +444,7 @@ var story = {
         "width": 163.5558319091797,
         "height": 21.33333396911621
        },
-       "index": 25,
+       "index": 27,
        "page": 7
       },
       {
@@ -434,7 +454,7 @@ var story = {
         "width": 181.33360290527344,
         "height": 21.33333396911621
        },
-       "index": 26,
+       "index": 28,
        "page": 6
       },
       {
@@ -444,7 +464,7 @@ var story = {
         "width": 233.77777099609375,
         "height": 35.55555725097656
        },
-       "index": 27,
+       "index": 29,
        "page": 1
       },
       {
@@ -454,8 +474,8 @@ var story = {
         "width": 234,
         "height": 52
        },
-       "index": 28,
-       "url": "https://www.google.com.br/"
+       "index": 30,
+       "url": "https://www.plataforma.ze.express/app/sales"
       },
       {
        "rect": {
@@ -464,7 +484,7 @@ var story = {
         "width": 234,
         "height": 52
        },
-       "index": 29,
+       "index": 31,
        "url": "https://conteudo.ze.express/abf-c"
       }
      ],
@@ -480,7 +500,7 @@ var story = {
       "width": 254.01416015625,
       "height": 48.583526611328125
      },
-     "index": 22,
+     "index": 24,
      "page": 2
     },
     {
@@ -490,7 +510,7 @@ var story = {
       "width": 292.60498046875,
       "height": 48.583526611328125
      },
-     "index": 23,
+     "index": 25,
      "page": 1
     }
    ],
@@ -519,7 +539,7 @@ var story = {
       "width": 241,
       "height": 20
      },
-     "index": 30,
+     "index": 32,
      "page": 0
     }
    ],
@@ -549,7 +569,7 @@ var story = {
       "width": 241,
       "height": 20
      },
-     "index": 31,
+     "index": 33,
      "page": 1
     }
    ],
@@ -597,7 +617,7 @@ var story = {
         "width": 146.96142578125,
         "height": 22.266881942749023
        },
-       "index": 32,
+       "index": 34,
        "page": 8
       },
       {
@@ -607,7 +627,7 @@ var story = {
         "width": 163.8849639892578,
         "height": 21.376205444335938
        },
-       "index": 33,
+       "index": 35,
        "page": 7
       },
       {
@@ -617,7 +637,7 @@ var story = {
         "width": 114.89710998535156,
         "height": 22.266881942749023
        },
-       "index": 34,
+       "index": 36,
        "page": 1
       },
       {
@@ -627,8 +647,8 @@ var story = {
         "width": 234,
         "height": 52
        },
-       "index": 35,
-       "url": "https://www.google.com.br/"
+       "index": 37,
+       "url": "https://www.plataforma.ze.express/app/sales"
       },
       {
        "rect": {
@@ -637,7 +657,7 @@ var story = {
         "width": 234,
         "height": 52
        },
-       "index": 36,
+       "index": 38,
        "url": "https://conteudo.ze.express/abf-c"
       }
      ],
@@ -654,9 +674,9 @@ var story = {
    "id": "10:2887",
    "groupID": "0:1",
    "transAnimType": 0,
-   "title": "NIVEL DE SERVIÇO",
-   "image": "nivel-de-serviço.png",
-   "imageFixedLess": "fl/nivel-de-serviço.png",
+   "title": "NIVEL DE SERVICO",
+   "image": "nivel-de-servico.png",
+   "imageFixedLess": "fl/nivel-de-servico.png",
    "index": 7,
    "width": 1440,
    "height": 1240,
@@ -689,7 +709,7 @@ var story = {
         "width": 146.6666717529297,
         "height": 22.22222137451172
        },
-       "index": 37,
+       "index": 39,
        "page": 8
       },
       {
@@ -699,7 +719,7 @@ var story = {
         "width": 181.3333282470703,
         "height": 21.33333396911621
        },
-       "index": 38,
+       "index": 40,
        "page": 6
       },
       {
@@ -709,7 +729,7 @@ var story = {
         "width": 114.66666412353516,
         "height": 22.22222137451172
        },
-       "index": 39,
+       "index": 41,
        "page": 1
       },
       {
@@ -719,8 +739,8 @@ var story = {
         "width": 234,
         "height": 52
        },
-       "index": 40,
-       "url": "https://www.google.com.br/"
+       "index": 42,
+       "url": "https://www.plataforma.ze.express/app/sales"
       },
       {
        "rect": {
@@ -729,11 +749,11 @@ var story = {
         "width": 234,
         "height": 52
        },
-       "index": 41,
+       "index": 43,
        "url": "https://conteudo.ze.express/abf-c"
       }
      ],
-     "image": "nivel-de-serviço-0.png",
+     "image": "nivel-de-servico-0.png",
      "mskH": 960
     }
    ],
@@ -781,7 +801,7 @@ var story = {
         "width": 163.55555725097656,
         "height": 21.33333396911621
        },
-       "index": 43,
+       "index": 45,
        "page": 7
       },
       {
@@ -791,7 +811,7 @@ var story = {
         "width": 181.3333282470703,
         "height": 21.33333396911621
        },
-       "index": 44,
+       "index": 46,
        "page": 6
       },
       {
@@ -801,7 +821,7 @@ var story = {
         "width": 114.66666412353516,
         "height": 22.22222137451172
        },
-       "index": 45,
+       "index": 47,
        "page": 1
       },
       {
@@ -811,8 +831,8 @@ var story = {
         "width": 234,
         "height": 52
        },
-       "index": 46,
-       "url": "https://www.google.com.br/"
+       "index": 48,
+       "url": "https://www.plataforma.ze.express/app/sales"
       },
       {
        "rect": {
@@ -821,7 +841,7 @@ var story = {
         "width": 234,
         "height": 52
        },
-       "index": 47,
+       "index": 49,
        "url": "https://conteudo.ze.express/abf-c"
       }
      ],
@@ -837,7 +857,7 @@ var story = {
       "width": 1068,
       "height": 77.90117645263672
      },
-     "index": 42,
+     "index": 44,
      "page": 9
     }
    ],
@@ -884,7 +904,7 @@ var story = {
         "width": 233.77777099609375,
         "height": 35.55555725097656
        },
-       "index": 49,
+       "index": 53,
        "page": 8
       },
       {
@@ -894,7 +914,7 @@ var story = {
         "width": 163.55555725097656,
         "height": 21.33333396911621
        },
-       "index": 50,
+       "index": 54,
        "page": 7
       },
       {
@@ -904,7 +924,7 @@ var story = {
         "width": 181.3333282470703,
         "height": 21.33333396911621
        },
-       "index": 51,
+       "index": 55,
        "page": 6
       },
       {
@@ -914,7 +934,7 @@ var story = {
         "width": 114.66666412353516,
         "height": 22.22222137451172
        },
-       "index": 52,
+       "index": 56,
        "page": 1
       },
       {
@@ -924,8 +944,8 @@ var story = {
         "width": 234,
         "height": 52
        },
-       "index": 53,
-       "url": "https://www.google.com.br/"
+       "index": 57,
+       "url": "https://www.plataforma.ze.express/app/sales"
       },
       {
        "rect": {
@@ -934,7 +954,7 @@ var story = {
         "width": 234,
         "height": 52
        },
-       "index": 54,
+       "index": 58,
        "url": "https://conteudo.ze.express/abf-c"
       }
      ],
@@ -945,18 +965,82 @@ var story = {
    "links": [
     {
      "rect": {
+      "x": 1143.11474609375,
+      "y": 388.983154296875,
+      "width": 227.89451599121094,
+      "height": 34.64785385131836
+     },
+     "index": 50,
+     "page": 10,
+     "pageType": "overlay",
+     "pageOverlayPin": 1
+    },
+    {
+     "rect": {
+      "x": 326,
+      "y": 361,
+      "width": 1065,
+      "height": 91
+     },
+     "index": 51,
+     "page": 10,
+     "pageType": "overlay",
+     "pageOverlayPin": 1
+    },
+    {
+     "rect": {
       "x": 354,
       "y": 61,
       "width": 28,
       "height": 28
      },
-     "index": 48,
+     "index": 52,
      "page": 8
     }
    ],
    "overlayPinPage": 6,
    "overlayOutsideClickIgnore": true,
    "layout": null
+  },
+  {
+   "id": "13:698",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "lista de produtos para abastecer",
+   "image": "lista-de-produtos-para-abastecer.png",
+   "index": 10,
+   "width": 790,
+   "height": 725,
+   "x": 6446,
+   "y": 1794,
+   "type": "modal",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 738,
+      "y": 32,
+      "width": 20,
+      "height": 20
+     },
+     "index": 59,
+     "action": "CLOSE"
+    },
+    {
+     "rect": {
+      "x": 531,
+      "y": 659,
+      "width": 227,
+      "height": 34
+     },
+     "index": 60,
+     "page": 9
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": false,
+   "layout": null,
+   "showShadow": true
   }
  ],
  "groups": [
@@ -966,6 +1050,6 @@ var story = {
   }
  ],
  "startPageIndex": 1,
- "totalImages": 10,
+ "totalImages": 11,
  "backColor": "#FFFFFF"
 }
